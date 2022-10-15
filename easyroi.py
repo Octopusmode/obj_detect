7c1e57a3ff7f37a46e83e52342fd5c048d52e37b
Osminin Max <osmininmaks52@gmail.com>
2022-10-15 13:19:43 +0300

    use fire_detector.onnx

--- easyroi.py
+++ easyroi.py
@@ -1,7 +1,10 @@
 from EasyROI import EasyROI
 import cv2
-from sympy import Point, Polygon
-from shapely import difference
+import numpy as np
+from matplotlib import pyplot as plt
+
+# from sympy import Point, Polygon
+from shapely.geometry import Polygon
 
 roi_helper = EasyROI(verbose=True)
 
@@ -18,18 +21,41 @@ verticles_dict2 = polygon_roi["roi"][1]["vertices"]
 # p5, p6, p7 = map(Point, [(3, 2), (1, -1), (0, 2)])
   
 # creating polygons using Polygon()
-poly1 = Polygon(*verticles_dict1)
-poly2 = Polygon(*verticles_dict2)
+poly1 = Polygon(verticles_dict1)
+poly2 = Polygon(verticles_dict2)
 
 # poly2 = Polygon(p5, p6, p7)
   
 # using intersection()
-isIntersection = poly1.intersection(poly2)
+# isIntersection = poly1.intersection(poly2)
   
 # print(poly1)
 # print(poly2)
 
 diff = poly2.difference(poly1)  # or difference = polygon2 - polygon1
+shapes = np.zeros_like(frame, np.uint8)
 
-print(isIntersection)
-print(diff)
+int_coords = lambda x: np.array(x).round().astype(np.int32)
+exterior = [int_coords(diff.exterior.coords)]
+poly1_ex = [int_coords(poly1.exterior.coords)]
+# poly2_ex = [int_coords(poly2.exterior.coords)]
+
+cv2.drawContours(shapes, exterior, -1, (0, 255, 255), -1)
+cv2.drawContours(shapes, poly1_ex, -1, (255, 0, 255), -1)
+# cv2.drawContours(shapes, poly2_ex, -1, (255, 255, 0), -1)
+
+perc = (poly2.area / 100 * (poly2.area - diff.area))
+
+# Generate output by blending image with shapes image, using the shapes
+# images also as mask to limit the blending to those parts
+out = frame.copy()
+alpha = 0.5
+mask = shapes.astype(bool)
+out[mask] = cv2.addWeighted(frame, alpha, shapes, 1 - alpha, 0)[mask]
+
+print(frame.shape[0])
+print(frame.shape[1])
+
+plt.imshow(out)
+plt.title("matplotlib imshow")
+plt.show()
--- main.py
+++ main.py
@@ -9,10 +9,13 @@ fc = 0
 FPS = 0
 
 #Opencv DNN
-net = cv2.dnn.readNet(r'dnn_model\yolov4-tiny.weights', r'dnn_model\yolov4-tiny.cfg')
+# net = cv2.dnn.readNet(r'dnn_model\yolov4-tiny.weights', r'dnn_model\yolov4-tiny.cfg')
 # net = cv2.dnn.readNet(r'dnn_model\yolov7-tiny.weights', r'dnn_model\yolov7-tiny.cfg')
+
+net = cv2.dnn.readNetFromONNX('fire_detector.onnx')
+
 model = cv2.dnn_DetectionModel(net)
-model.setInputParams(size=(320,320), scale=1/255)
+model.setInputParams(size=(416,416), scale=1/255)
 
 # Load class lists
 classes = []
@@ -25,16 +28,26 @@ IMG_WIDTH = 1920
 IMG_HEIGHT = 1080
 
 # Initialize camer/stream
-cap = cv2.VideoCapture('video_park.mp4')
+# cap = cv2.VideoCapture('video_park.mp4')
+cap = cv2.VideoCapture('rtsp://vidanalitic:q23NYz9SXQ@192.168.103.48:554/RVi/1/1')
+# cap = cv2.VideoCapture('rtsp://vidanalitic:q23NYz9SXQ@192.168.103.48:554/RVi/1/1')
+# rtsp://vidanalitic:q23NYz9SXQ@192.168.103.48:554/PSIA/streaming/channels/101
+# cap = cv2.VideoCapture(r'video_park.mp4')
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, IMG_WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT)
 first = False
+count = 1000
 
+def contains(x1, y1, x2, y2, ox1=120, oy1=70, ox2=1580, oy2=880):
+   return x1 < ox1 < ox2 < x2 and y1 < oy1 < oy2 < y2
 
 while True:
     ret, frame = cap.read()
+    if type(frame) == 'NoneType':
+        frame = to_save.copy()
+    else:
+        to_save = frame.copy()
     frame_net = cv2.bilateralFilter(frame, 5, 20, 20)
-    
     # if not first:
     #     cv2.imshow('filter', frame_net)
     #     first = True
@@ -49,20 +62,29 @@ while True:
     fps_disp = "FPS: "+str(round(FPS))[:5]
     
     # Object detection
-    (class_ids, scores, bboxes) = model.detect(frame_net)
+    # (class_ids, scores, bboxes) = model.detect(frame_net)
     
     i = 0
     
-    for class_id, score, bbox in zip(class_ids, scores, bboxes):
-            (x, y, w, h) = bbox
-            i += 1
-            # print(x, y, w, h)
-            if score > 0.2 and class_id == 0:
-                cv2.putText(frame, str(classes[class_id]) + str(i), (x, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (200, 0, 50), 2)
-                cv2.putText(frame, str(score)[:4], (x, y + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 200), 2)
-                cv2.rectangle(frame, (x, y),(x+w, y + h), (200, 0, 0), 3)
+    # for class_id, score, bbox in zip(class_ids, scores, bboxes):
+    #         (x, y, w, h) = bbox
+    #         i += 1
+    #         # print(x, y, w, h)
+    #         if score > 0.25: # and class_id == 56:
+    #             cv2.putText(frame, str(classes[class_id]) + str(i), (x, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (200, 0, 50), 2)
+    #             cv2.putText(frame, str(score)[:4], (x, y + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 200), 2)
+    #             cv2.rectangle(frame, (x, y),(x+w, y + h), (200, 0, 0), 3)
+                
+    #             # key_pressed = cv2.waitKey(1)
+                
+    #             # ih, iw = to_save.shape[:2]
+    #             # # if count % 100 == 0 and x+w < 1500 and y+h < 900:
+    #             # if key_pressed == 99:
+    #             #     cv2.imwrite(r"out\frame%d.jpg" % count, to_save)     # save frame as JPEG file
+    #             #     count += 1 
+                    
     
-    cv2.putText(frame, fps_disp, (7, 70), cv2.FONT_HERSHEY_DUPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+    cv2.putText(frame, fps_disp, (7, 70), cv2.FONT_HERSHEY_DUPLEX, 1, (100, 255, 0), 3, cv2.LINE_AA)
     
     # print('class_ids', class_ids)
     # print('score', scores)
